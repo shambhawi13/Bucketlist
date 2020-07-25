86835b84c8c96c763a82a83b44515e87de46e218
Sham <shambhawi.kumari1393@gmail.com>
2020-07-25 13:04:06 -0700

    added heroku connection db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "bucketlist_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "bucketlist_db"
+// });
+
+// Heroku
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function(err) {
